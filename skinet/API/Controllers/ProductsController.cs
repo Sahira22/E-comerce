b205483aa001bf0df0215f6abea46833abf46abc
Sahira22 <60494004+Sahira22@users.noreply.github.com>
2021-04-10 10:48:50 -0400

    Section 2 finished

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,4 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace API.Controllers
 {
@@ -7,17 +15,26 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public int  GetProducts(){
+        private readonly StoreContext _context;
 
-            return 22;
+        public ProductsController(StoreContext context)
+        {
+            _context= context;
+        }
+
+        [HttpGet]
+         public async Task<ActionResult<List<Product>>> GetProducts(){
+
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public int  GetProduct(int id){
-
-            return 1;
+       public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
         }
+
 
 
     }
