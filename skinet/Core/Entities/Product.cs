cf4ebf974c540ed7d5f32ac23ae5c0e0b88b1a5a
Sahira22 <60494004+Sahira22@users.noreply.github.com>
2021-04-10 22:41:27 -0400

    Section 3  -25
    Section 3  -25

--- skinet/Core/Entities/Product.cs
+++ skinet/Core/Entities/Product.cs
@@ -6,5 +6,7 @@ namespace Core.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        //Aqui van lo campos como descripcin, url etc, en el repositorio skinet se pueden visualiza. Solo es copy y paste
     }
-cd 
+}
